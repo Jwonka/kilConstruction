4304004b1ee7d7e2f3aae27cd48ff8c8027fb413
Josh Werlein <jwerlein2@student.cvtc.edu>
2025-11-04 19:43:38 -0600

    hook up photos

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,4 @@ import { defineConfig } from 'astro/config';
 const site = process.env.ASTRO_SITE || 'https://kilcon.work';
 const base = process.env.ASTRO_BASE || '/';
 
-export default defineConfig({ site, base });
+export default defineConfig({ site, base, output: 'static',});
