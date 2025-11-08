5c628ad7829b8b2f9169517e740b6989d78a66cd
Josh Werlein <jwerlein2@student.cvtc.edu>
2025-11-08 10:55:33 -0600

    fix photo images

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,4 @@ import { defineConfig } from 'astro/config';
 const site = process.env.ASTRO_SITE || 'https://kilcon.work';
 const base = process.env.ASTRO_BASE || '/';
 
-export default defineConfig({ site, base, output: 'static',});
+export default defineConfig({ site: 'https://kilcon.work', base, output: 'static',});
