676b5eaa9a1eeea9ce0952698833879fcfe79987
Josh Werlein <jwerlein2@student.cvtc.edu>
2025-11-12 13:52:05 -0600

    add admin pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 const site = process.env.ASTRO_SITE || 'https://kilcon.work';
 const base = process.env.ASTRO_BASE || '/';
 
-export default defineConfig({ site: 'https://kilcon.work', base, output: 'static',});
+export default defineConfig({
+    site: 'https://kilcon.work',
+    base,
+    output: 'server',
+    adapter: cloudflare(),
+});
