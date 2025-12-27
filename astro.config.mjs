76165d7ef24b3ab37c977c13617bae2b44a3c805
JWonka <jjwerlein@gmail.com>
2025-12-27 13:18:20 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ const site = process.env.ASTRO_SITE || 'https://kilcon.work';
 const base = process.env.ASTRO_BASE || '/';
 
 export default defineConfig({
+    trailingSlash: "never",
     site: 'https://kilcon.work',
     base,
     output: 'server',
